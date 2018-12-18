50899f8b2bba3c503ffb34aeedf47c0dbb082452
Edvin Lellhame <edvinlellhame@edvins-macbook-pro.local>
2018-12-17 20:34:38 -0800

    Initial Commit

---
+++ Bus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bus
+//
+//  Created by Edvin Lellhame on 12/17/18.
+//  Copyright © 2018 Edvin Lellhame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
